e2342e94dd983159d7a637bdf8043f18759e1390
Poomrapee-chsk <poomrapeecharoensak@gmail.com>
2025-11-30 17:59:25 +0700

    added score display on screen

--- shaders/text.fs
+++
@@ -1,9 +0,0 @@
-#version 330 core
-out vec4 FragColor;
-
-uniform vec3 textColor;
-
-void main()
-{
-    FragColor = vec4(textColor, 1.0);
-}
--- src/game.cpp
+++ src/game.cpp
@@ -2,12 +2,13 @@
 #include "GLFW/glfw3.h"
 #include "glm/common.hpp"
 #include "glm/trigonometric.hpp"
-#include <array>
 #include <cmath>
 #include <cstdlib>
+#include <ft2build.h>
 #include <stb_image.h>
-#include <stb_easy_font.h>
+#include FT_FREETYPE_H
 
+#include <glad/glad.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/quaternion.hpp>
@@ -24,6 +25,8 @@
 #include <vector>
 
 void renderCube();
+void RenderText(Shader &s, std::string text, float x, float y, float scale,
+                glm::vec3 color);
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void mouse_callback(GLFWwindow *window, double xpos, double ypos);
 void scroll_callback(GLFWwindow *window, double xoffset, double yoffset);
@@ -77,6 +80,7 @@ float lastFrame = 0.0f;
 float lastToggleTime = 0.0f;
 const float toggleCooldown = 0.3f;
 
+// Math functions
 glm::vec3 ForwardFromEuler(float yaw, float pitch) {
   // Convert degrees to radians
   float yawRad = glm::radians(yaw);
@@ -281,9 +285,16 @@ public:
     std::cout << "Loaded " << indices.size() << " indices" << std::endl;
   }
   // Helper function for plane
-  glm::vec3 get_random_start_location() {
-    return glm::vec3(-width + ((float)rand() / RAND_MAX * (2 * width)), 50.0f,
-                     -height + ((float)rand() / RAND_MAX * (2 * height)));
+  glm::vec3 GetRandomStartLocation() {
+    // Get random spawn location on map (X, Y) as (x, _, z)
+    float x = -width + ((float)rand() / RAND_MAX * (2 * width));
+    float z = -height + ((float)rand() / RAND_MAX * (2 * height));
+
+    // Sample the height from the coordinates
+    float y = GetHeightAt(x, z);
+    y += 50;
+
+    return glm::vec3(x, y, z);
   }
 
   void DrawDepth(Shader &depthShader, glm::mat4 model) {
@@ -495,14 +506,27 @@ struct Plane {
     Speed = glm::clamp(Speed, MIN_SPEED, MAX_SPEED);
   }
 
-  void DampenRoll(float dt) {
-    // auto-level: move roll toward zero
-    if (Roll > 0.1f)
-      RollRight(dt);
-    else if (Roll < -0.1f)
-      RollLeft(dt);
-    else
-      Roll = 0.0f;
+  /* Getters */
+  string GetSpeed() {
+      return std::to_string(int(round(Speed))) + " kts";
+  }
+
+  string GetAltitude() {
+      std::stringstream altitude;
+      altitude << std::fixed << std::setprecision(2) << Position.y << " feet";
+      return altitude.str();
+  }
+
+  string GetRoll() {
+      return std::to_string(Roll);
+  }
+
+  string GetPitch() {
+      return std::to_string(Pitch);
+  }
+
+  string GetYaw() {
+      return std::to_string(Yaw);
   }
 
   // Returns model matrix for rendering
@@ -516,7 +540,7 @@ struct Plane {
   }
 
   void Reset() {
-    Position = glm::vec3(Terrain->get_random_start_location());
+    Position = glm::vec3(Terrain->GetRandomStartLocation());
     Pitch = 0.0f;
     Roll = 0.0f;
     Yaw = 0.0f;
@@ -528,7 +552,7 @@ struct Plane {
 
   // Update: move forward and handle ground collision
   void Update(float dt) {
-    if (dt <= 0.0f || dt > 0.1f)
+    if (gameOver || dt <= 0.0f || dt > 0.1f)
       return;
 
     // Roll influences yaw (banking turns)
@@ -558,14 +582,13 @@ struct Plane {
       float groundY = Terrain->GetHeightAt(Position.x, Position.z);
       float minY = groundY + GroundClearance;
       if (Position.y < minY) {
-        // When hitting the ground, reset
-        Reset();
+          gameOver = true;
       }
     }
   }
 };
 
-// ===== HELPER FUNCTIONS =====
+// -------- Helper functions for balloons -----
 glm::vec3 GetRandomBalloonSpawnPosition() {
   float x =
       BALLOON_SPAWN_MIN_X +
@@ -614,6 +637,16 @@ void ResetGame(Plane &player, std::vector<Balloon> &balloons,
   }
 }
 
+/* Font Character */
+struct Character {
+  unsigned int TextureID; // ID handle of the glyph texture
+  glm::ivec2 Size;        // Size of glyph
+  glm::ivec2 Bearing;     // Offset from baseline to left/top of glyph
+  unsigned int Advance;   // Offset to advance to next glyph
+};
+std::map<char, Character> Characters;
+unsigned int fontVAO, fontVBO;
+
 int main() {
   srand(static_cast<unsigned>(time(nullptr)));
 
@@ -650,6 +683,92 @@ int main() {
     std::cout << "Failed to initialize GLAD" << std::endl;
     return -1;
   }
+
+  /* Setup font shader */
+  Shader fontShader("shaders/font.vs", "shaders/font.fs");
+  glm::mat4 fontProjection = glm::ortho(0.0f, static_cast<float>(SCR_WIDTH),
+                                        0.0f, static_cast<float>(SCR_HEIGHT));
+  fontShader.use();
+  fontShader.setInt("text", 0);
+  glUniformMatrix4fv(glGetUniformLocation(fontShader.ID, "projection"), 1,
+                     GL_FALSE, glm::value_ptr(fontProjection));
+
+  /* Font */
+  FT_Library ft;
+  if (FT_Init_FreeType(&ft)) {
+    std::cout << "ERROR::FREETYPE: Could not init FreeType Library"
+              << std::endl;
+    return -1;
+  }
+
+  /* Try to see if font exists */
+  std::string font_name = FileSystem::getPath("resources/fonts/modern_dos.ttf");
+
+  if (font_name.empty()) {
+    std::cout << "ERROR::FREETYPE: Failed to load font_name" << std::endl;
+    return -1;
+  }
+
+  // load font as a face
+  FT_Face face;
+  if (FT_New_Face(ft, font_name.c_str(), 0, &face)) {
+    std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;
+    return -1;
+  } else {
+    // set size to load glyphs as
+    FT_Set_Pixel_Sizes(face, 0, 48);
+
+    // disable byte-alignment restriction
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+    // load first 128 characters of ASCII set
+    for (unsigned char c = 0; c < 128; c++) {
+      // Load character glyph
+      if (FT_Load_Char(face, c, FT_LOAD_RENDER)) {
+        std::cout << "ERROR::FREETYTPE: Failed to load Glyph" << std::endl;
+        continue;
+      }
+      // generate texture
+      unsigned int fontTexture;
+      glGenTextures(1, &fontTexture);
+      glBindTexture(GL_TEXTURE_2D, fontTexture);
+      glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, face->glyph->bitmap.width,
+                   face->glyph->bitmap.rows, 0, GL_RED, GL_UNSIGNED_BYTE,
+                   face->glyph->bitmap.buffer);
+      // set texture options
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+      // now store character for later use
+      Character character = {
+          fontTexture,
+          glm::ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
+          glm::ivec2(face->glyph->bitmap_left, face->glyph->bitmap_top),
+          static_cast<unsigned int>(face->glyph->advance.x)};
+      Characters.insert(std::pair<char, Character>(c, character));
+    }
+    glBindTexture(GL_TEXTURE_2D, 0);
+  }
+
+  /* Clear resource */
+  FT_Done_Face(face);
+  FT_Done_FreeType(ft);
+
+  /* Enable blending to render fonts */
+  glEnable(GL_BLEND);
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+  /* Reserve buffer for fonts */
+  glGenVertexArrays(1, &fontVAO);
+  glGenBuffers(1, &fontVBO);
+  glBindVertexArray(fontVAO);
+  glBindBuffer(GL_ARRAY_BUFFER, fontVBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6 * 4, NULL, GL_DYNAMIC_DRAW);
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), 0);
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+  glBindVertexArray(0);
 
   // configure global opengl state
   // -----------------------------
@@ -846,8 +965,6 @@ int main() {
     // input
     // -----
     processInput(window);
-
-    // --- Plane keyboard controls ---
     if (!gameOver) {
       if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
         player.PitchUp(deltaTime);
@@ -917,7 +1034,9 @@ int main() {
     }
 
     // Update plane physics
-    player.Update(deltaTime);
+    if (!gameOver) {
+      player.Update(deltaTime);
+    }
 
     // Camera: follow behind the plane
     if (followPlane) {
@@ -1077,15 +1196,36 @@ int main() {
     renderCube();
     glDepthFunc(GL_LESS); // set depth function back to default
 
+    ///////// Text Rendering Section //////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
+    glDisable(GL_DEPTH_TEST);
+
+    /* Render Score */
+    RenderText(fontShader,
+               "Score: " + std::to_string(playerScore) +
+                   (gameOver ? "  |  GAME OVER" : ""),
+               50.0f, SCR_HEIGHT - 150.0f, 0.8f, glm::vec3(1.0f));
+
+    /* Render Speed */
+    RenderText(fontShader, player.GetSpeed(), SCR_WIDTH - 350.0f,
+               200.0f, 0.8f, glm::vec3(1.0f));
+
+    /* Render Altitude */
+    RenderText(fontShader, player.GetAltitude(), SCR_WIDTH - 350.0f,
+               300.0f, 0.8f, glm::vec3(1.0f));
+
+    glEnable(GL_DEPTH_TEST);
+    ///////////////////////////////////////////////////////////////////////////
+
     // Display score and status in window title
-    std::stringstream ss;
-    ss << "Plane Game - Score: " << playerScore << " | Speed: " << std::fixed
-       << std::setprecision(1) << player.Speed;
-    if (gameOver) {
-      ss << " | GAME OVER - Press R to Restart";
-    } else {
-      ss << " | Press R to Restart";
-    }
+    // std::stringstream ss;
+    // ss << "Plane Game - Score: " << playerScore << " | Speed: " << std::fixed
+    //    << std::setprecision(1) << player.Speed;
+    // if (gameOver) {
+    //   ss << " | GAME OVER - Press R to Restart";
+    // } else {
+    //   ss << " | Press R to Restart";
+    // }
 
     // glfw: swap buffers and poll IO events (keys pressed/released, mouse
     // moved etc.)
@@ -1297,4 +1437,52 @@ void renderCube() {
   glBindVertexArray(cubeVAO);
   glDrawArrays(GL_TRIANGLES, 0, 36);
   glBindVertexArray(0);
+}
+
+// Render line of text
+void RenderText(Shader &shader, std::string text, float x, float y, float scale,
+                glm::vec3 color) {
+  // activate corresponding render state
+  shader.use();
+  glUniform3f(glGetUniformLocation(shader.ID, "textColor"), color.x, color.y,
+              color.z);
+  glActiveTexture(GL_TEXTURE0);
+  glBindVertexArray(fontVAO);
+
+  // iterate through all characters
+  std::string::const_iterator c;
+  for (c = text.begin(); c != text.end(); c++) {
+    Character ch = Characters[*c];
+
+    float xpos = x + ch.Bearing.x * scale;
+    float ypos = y - (ch.Size.y - ch.Bearing.y) * scale;
+
+    float w = ch.Size.x * scale;
+    float h = ch.Size.y * scale;
+    // update VBO for each character
+    float vertices[6][4] = {
+        {xpos, ypos + h, 0.0f, 0.0f},    {xpos, ypos, 0.0f, 1.0f},
+        {xpos + w, ypos, 1.0f, 1.0f},
+
+        {xpos, ypos + h, 0.0f, 0.0f},    {xpos + w, ypos, 1.0f, 1.0f},
+        {xpos + w, ypos + h, 1.0f, 0.0f}};
+    // render glyph texture over quad
+    glBindTexture(GL_TEXTURE_2D, ch.TextureID);
+    // update content of VBO memory
+    glBindBuffer(GL_ARRAY_BUFFER, fontVBO);
+    glBufferSubData(
+        GL_ARRAY_BUFFER, 0, sizeof(vertices),
+        vertices); // be sure to use glBufferSubData and not glBufferData
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    // render quad
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+    // now advance cursors for next glyph (note that advance is number of 1/64
+    // pixels)
+    x += (ch.Advance >> 6) *
+         scale; // bitshift by 6 to get value in pixels (2^6 = 64 (divide amount
+                // of 1/64th pixels by 64 to get amount of pixels))
+  }
+  glBindVertexArray(0);
+  glBindTexture(GL_TEXTURE_2D, 0);
 }
